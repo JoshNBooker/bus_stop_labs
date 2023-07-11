c1bc7fa54446abbc8ce2df283a7dabbf64ba6aca
Josh Booker <josh.booker2@gmail.com>
2023-07-11 14:52:41 +0100

    tried to make it a bit easier to read

--- app.py
+++ app.py
@@ -2,35 +2,31 @@ from person import Person
 from bus import Bus
 from bus_stop import BusStop
 
+# PART A
 bus1 = Bus(1, "Brunswick Road")
 
 print(f"the bus has left.{bus1.drive()}")
 
-print(bus1.passenger_number_check())
+# PART B
+initial_passengers = bus1.passenger_number_check()
+print(f"there are initially {initial_passengers} passengers on the bus")
 
 bus1.pick_up("John")
 print(bus1.passengers)
 
-print(bus1.passenger_number_check())
-
-bus1.pick_up("Mary")
+passengers_currently_on_bus = (bus1.passenger_number_check())
+print(f"there are currently {passengers_currently_on_bus} passengers on the bus")
 
 bus1.drop_off("John")
 
-print(bus1.passenger_number_check())
-
 bus1.empty()
-print(bus1.passenger_number_check())
+
+# PART C
 
 bus_stop1 = BusStop("King's Road")
 
 bus_stop1.queue_clear()
 print(len(bus_stop1.queue))
-
-
-bus_stop1.queue_add("Janie")
-
-bus_stop1.queue_add("Fred")
 
 print(bus_stop1.queue)
 
